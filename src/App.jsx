208c8cc5dae2d369030d774f522684d55d9aa1a3
Septian Adi Pratama <septianadip7998@gmail.com>
2025-03-11 22:05:02 +0700

    tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+const App = () => {
+  const [count,setCount] = useState(0)
 
-function App() {
-  const [count, setCount] = useState(0)
-
+  const HandleClick = () => {
+    setCount(count + 1)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className=" text-white">
+      <div className=" justify-center items-center grid space-y-5 mt-52">
+         <p className=" text-center">Extension Creator <a href="https://www.instagram.com/nandakaws/" className=" duration-200 hover:text-purple-500">NandaKaws</a></p>
+         <h1 className=" text-purple-500 text-5xl font-bold  "><span className=" text-white">Lets</span>code!</h1>
+         <button onClick={HandleClick} className=" rounded-lg font-bold text-xl bg-purple-500">Click</button>
+         <h1 className=" text-center font-bold  ">{count}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
