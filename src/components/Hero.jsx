69eaed7c3ba9029f42c10ab7c50d925d064de738
Septian Adi Pratama <septianadip7998@gmail.com>
2025-03-12 01:31:20 +0700

    hero need image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import hero from "../assets/Hero-section.png"
+import React from "react";
+import hero from "../assets/Hero-section.png";
+import { IoArrowForwardSharp } from "react-icons/io5";
 
 const Hero = () => {
   return (
-    <div>
-        <img src={hero} alt="hero"/>
+    <div
+      className="relative flex items-center h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${hero})` }}
+    >
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-6 md:px-12">
+        {/* Teks Hero */}
+        <div className="text-white max-w-lg">
+          <h1 className="text-4xl font-bold mb-4">
+            Get More Done with Whitespace
+          </h1>
+          <p className="text-lg mb-6">
+            Project management software that enables your teams to collaborate,
+            plan, analyze, and manage everyday tasks.
+          </p>
+          <button className="flex items-center gap-2 bg-[#4F9CF9] text-sm px-4 py-2 rounded">
+            Try Whitespace free <IoArrowForwardSharp />
+          </button>
+        </div>
+      </div>
+      <div className="">
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
